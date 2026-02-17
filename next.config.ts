61ce95437a67c6f785bb7d63efacab2e857e4f3a
vince115 <vince115@gmail.com>
2026-02-17 09:29:20 +0800

    feat: setup GitHub Pages static export and workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "next-book";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 靜態輸出到 /out
+  basePath: `/${repo}`, // 讓路由在 /next-book 下工作
+  images: { 
+    unoptimized: true  // GitHub Pages 必須關掉 Image Optimization
+  },
 };
 
 export default nextConfig;
